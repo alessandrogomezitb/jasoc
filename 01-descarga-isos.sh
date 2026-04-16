1aa7fc3baed7bf2e8ff541ffbf4cc3e3f0f29451
alessandrogomezitb <mario.moncada.7e9@itb.cat>
2026-04-16 12:16:36 +0200

    Descarga de ISOS necesarias

---
+++ 01-descarga-isos.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Iniciando descarga de ISOs necesarias (Fase 1)..."
+
+# Directorio por defecto de Proxmox para las ISOs
+ISO_DIR="/var/lib/vz/template/iso"
+
+# URLs de descarga
+PFSENSE_URL="https://pinguin.dinus.ac.id/iso/pfSense/iso/pfSense-CE-2.7.2-RELEASE-amd64.iso.gz"
+DEBIAN_URL="https://cdimage.debian.org/debian-cd/current/amd64/iso-cd/debian-13.4.0-amd64-netinst.iso"
+#SI, ES UNA URL DE UNA UNIVERSIDAD, PERO ES QUE PFSENSE OBLIGA A REGISTRARSE PARA DESCARGAR ISOS AHORA
+
+echo "Descargando pfSense..."
+wget -q --show-progress -O $ISO_DIR/pfSense.iso.gz $PFSENSE_URL
+echo "Descomprimiendo pfSense..."
+gunzip -f $ISO_DIR/pfSense.iso.gz
+
+echo "Descargando Debian 13 (Netinst)..."
+wget -q --show-progress -O $ISO_DIR/debian-13.iso $DEBIAN_URL
+
+echo "¡Fase 1 completada! Las ISOs están listas en $ISO_DIR."
